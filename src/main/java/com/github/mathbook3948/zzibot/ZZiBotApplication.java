ec0502465739ee8d2dcb24a0043a2c203eb62f86
mathbook3948 <yuno.jung.07@gmail.com>
2025-06-30 21:40:40 +0900

    First Commit

---
+++ src/main/java/com/github/mathbook3948/zzibot/ZZiBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.mathbook3948.zzibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZZiBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZZiBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mathbook3948/zzibot/ZZiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mathbook3948.zzibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZZiBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
